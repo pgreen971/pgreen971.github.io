332b7e03432d54ce7400a465f261702e7a1e6f95
pgreen971 <pgnetmail@yahoo.com>
2024-11-20 20:00:37 -0500

    let's try

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// holdover
+function enquireProduct(specifiedElement, type) {
+    specifiedElement.onclick = ""
+    specifiedElement.style = "cursor: wait; background-color: rgb(150, 150, 150);"
+    specifiedElement.textContent = "Process"
+    let modHREF = ""
+    if (modHREF.indexOf("#")) {
+        for (var i = 0; i < modHREF.indexOf("#"); i++) {
+            modHREF += location.href[i]
+        }
+    } else {
+        modHREF = location.href
+    }
+    let win = window.open("mailto:pgnetmail@yahoo.com?subject=Automatic " + type + " Enquiry&body=" + modHREF + "#" + specifiedElement.id)
+    const timer = setInterval(() => {
+        if (win.closed) {
+            clearInterval(timer);
+            specifiedElement.textContent = "Enquired";
+            specifiedElement.style = "cursor: not-allowed; background-color: rgb(150, 150, 150);";
+        }
+    }, 500);
+}
